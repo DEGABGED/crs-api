bbb27a304a5e00ba4a714035d8b457ea6e654cac
Francis dela Cruz <franciszac.dlc@gmail.com>
2017-12-27 21:16:13 +0800

    Refactor out JSON (Chap2)

--- sched_api/urls.py
+++ sched_api/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from sched_api import views
 
 urlpatterns = [
     url(r'^subjects/$', views.subject_list),
     url(r'^subjects/(?P<pk>[0-9]+)/$', views.subject_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- sched_api/views.py
+++ sched_api/views.py
@@ -1,53 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from sched_api.models import Subject
 from sched_api.serializers import SubjectSerializer
 
 # Create your views here.
-@csrf_exempt
-def subject_list(request):
+@api_view(['GET', 'POST'])
+def subject_list(request, format=None):
     """
     List all code snippets, or create a new snippet.
     """
     if request.method == 'GET':
         subjects = Subject.objects.all()
         serializer = SubjectSerializer(subjects, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = SubjectSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def subject_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def subject_detail(request, pk, format=None):
     """
     Retrieve, update or delete a subject.
     """
     try:
         subject = Subject.objects.get(pk=pk)
     except Subject.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     # Subject object already obtained, now process according to request
     if request.method == 'GET':
         serializer = SubjectSerializer(subject)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SubjectSerializer(subject, data=data)
+        serializer = SubjectSerializer(subject, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         subject.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
